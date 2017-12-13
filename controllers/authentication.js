fe24a65f431ad9b603f9f5d68e4056544a848408
Andrew Murray <darbyguitars@gmail.com>
2017-12-13 10:49:40 -0600

    cvreated a route in the router folder that can create a user name or password

---
+++ controllers/authentication.js
@@ -0,0 +1,33 @@
+const User = require("../models/user");
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email,
+        password = req.body.password;
+
+  //  see if a user with the given email exists
+  User.findOne({ email: email }, (err, existingUser) => {
+    if (err) {
+      return next(err);
+    }
+
+    //If a the email is used return error
+    if (existingUser) {
+      return res.status(422).send({ error: "Email has been used" });
+    }
+
+    //If the email is does not exist create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(err => {
+      if (err) {
+        return next(err);
+      }
+        //  Respond to the request whether or not the user was created
+      res.json(user);
+
+    });
+  });
+};
